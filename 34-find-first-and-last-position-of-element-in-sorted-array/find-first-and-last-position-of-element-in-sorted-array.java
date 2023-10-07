34572f297e0ccf19160d1e8cf185160b57348cb4
Bodhisatwa Chakraborty <95414241+Bodhisatwa2002@users.noreply.github.com>
2023-10-07 22:36:10 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (38.53%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,38 +1,52 @@
 class Solution {
-       public int[] searchRange(int[] nums, int target){
-        int[] ans = {-1,-1};
-        //check for the first occurence if target first
-        int start= search(nums, target, true);
-        int end= search(nums,target, false);
-        ans[0] = start;
-        ans[1] = end;
-        return ans;
-    }
-    // this function just returns the index value of target
-    int search(int[] nums, int target, boolean findStartIndex) {
-        int ans = -1;
+    public int findFirstOccurrence(int[] nums, int target) {
         int start = 0;
         int end = nums.length - 1;
-        while(start <= end) {
-            // find the middle element
-//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
+        int first = -1;
+
+        while (start <= end) {
             int mid = start + (end - start) / 2;
 
-            if (target < nums[mid]) {
+            if (nums[mid] == target) {
+                first = mid;
+                end = mid - 1; // Continue searching for the first occurrence on the left side.
+            } else if (nums[mid] > target) {
                 end = mid - 1;
-            } else if (target > nums[mid]) {
-                start = mid + 1;
             } else {
-                // potential ans found
-                ans = mid;
-                if (findStartIndex) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
+                start = mid + 1;
             }
         }
-        return ans;
+
+        return first;
     }
 
+    public int findLastOccurrence(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        int last = -1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] == target) {
+                last = mid;
+                start = mid + 1; // Continue searching for the last occurrence on the right side.
+            } else if (nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+
+        return last;
+    }
+
+    public int[] searchRange(int[] nums, int target) {
+        int first = findFirstOccurrence(nums, target);
+        if (first == -1) {
+            return new int[]{-1, -1};
+        }
+        int last = findLastOccurrence(nums, target);
+        return new int[]{first, last};
+    }
 }
