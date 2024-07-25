a7ae1519e7068b357ca0057fb66112ebdc7daaaa
Bodhisatwa Chakraborty <95414241+Bodhisatwa2002@users.noreply.github.com>
2024-07-25 23:13:47 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (28.42%) - LeetSync

---
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode fast= head;
+        ListNode slow = head;
+        while(fast!=null && fast.next!= null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow){
+                ListNode p2 = head;
+                while(p2!=slow){
+                    p2=p2.next;
+                    slow=slow.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
